684fe74757a0293ef0ecab14131c13f8aa28bb65
ashpe <irashp@gmail.com>
2013-01-23 16:31:40 +1100

    create update-player

--- src/rpg/player.clj
+++ src/rpg/player.clj
@@ -5,9 +5,13 @@
     :gender nil
     :class  nil })
 
+(defn update-player
+  [key, value]
+  (def player
+    (assoc player key value)))
+
 (defn create-player! 
   "Create new player"
   [name, gender]
-  (def player
-    (assoc player :name name :gender gender)))
-
+  (update-player :name name)
+  (update-player :gender gender))
